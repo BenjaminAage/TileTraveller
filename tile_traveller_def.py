9268ce24dee61d9ac3264d7d752890e0d385299c
Benjamín_Aage <benjamin18@ru.is>
2018-09-18 17:45:24 +0000

    Handin

--- tile_traveller_def.py
+++ tile_traveller_def.py
@@ -4,46 +4,35 @@ def routes(direction, route):
       return True
   return False
       
-#       
-#       if direction == "N":
-#         y += 1
-#       elif direction == "E":
-#         x += 1
-#       elif direction == "S":
-#         y -= 1
-#       elif direction == "W":
-#         x -= 1
-#       break
-#   else:
-#     print("Not a valid direction!")
-# 
 
 def new_route(x, y):
-  if x2 != x or y2 != y:
-    if x == 1 and y == 1:
-      return "N"
-    elif x == 1 and y == 2:
-      return "NES"
-    elif x == 1 and y == 3:
-      return "ES"
-    elif x == 2 and y == 1:
-      return "N"
-    elif x == 2 and y == 2:
-      return "SW"
-    elif x == 2 and y == 3:
-      return "EW"
-    elif x == 3 and y == 1:
-      return ""
-    elif x == 3 and y == 2:
-      return "NS"
-    elif x == 3 and y == 3:
-      return "SW"
+  if x == 1 and y == 1:
+    return "N"
+  elif x == 1 and y == 2:
+    return "NES"
+  elif x == 1 and y == 3:
+    return "ES"
+  elif x == 2 and y == 1:
+    return "N"
+  elif x == 2 and y == 2:
+    return "SW"
+  elif x == 2 and y == 3:
+    return "EW"
+  elif x == 3 and y == 1:
+    return ""
+  elif x == 3 and y == 2:
+    return "NS"
+  elif x == 3 and y == 3:
+    return "SW"
+
+
+def printed(route):
+  if route != "":
     print("You can travel: ", end="")
 
-def printed():
   for i in range(len(route)):
       if i != 0:
-        print(" or", end="")
+        print(" or ", end="")
       if route[i] == "N":
         print("(N)orth", end="")
       elif route[i] == "E":
@@ -52,22 +41,30 @@ def printed():
         print("(S)outh", end="")
       elif route[i] == "W":
         print("(W)est", end="")
-    print()
-
-  print("Victory!")
+  print(".")
 
 
 x = 1
 y = 1
 route = "N"
+print("You can travel: (N)orth.")
 
-while True:
-  x2 = x
-  y2 = y
+while not (x == 3 and y == 1):
   direction = input("Direction: ").upper()
+  if routes(direction, route):
+    if direction == "N":
+      y += 1
+    if direction == "E":
+      x += 1
+    if direction == "S":
+      y -= 1
+    if direction == "W":
+      x -= 1
 
+    route = new_route(x, y)
+    printed(route)
+  else:
+      print("Not a valid direction!")
+      
 
-
-  
-  
-    
+print("Victory!")
